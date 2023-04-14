4ca23faa169f5879693098579daf91d59a4090bb
jpwf <64947903+jpwf@users.noreply.github.com>
2023-04-14 18:10:23 -0300

    Its working

--- ArduinoShortcut.py
+++ ArduinoShortcut.py
@@ -4,29 +4,29 @@ from time import sleep
 
 figma =' https://www.figma.com/files/recent?fuid=1073990296500635793'
 notion = 'https://www.notion.so/Instagram-do-JPWF-49eb407b3d1d4e94ac56381869248711'
-board = Arduino('/dev/ttyUSB1')
+board = Arduino('COM9')
 it = util.Iterator(board)
 it.start()
 
 
-figmaButton = board.get_pin('d:13:1')
-notionButton = board.get_pin('d:12:1')
+figmaButton = board.get_pin('d:13:i')
+notionButton = board.get_pin('d:12:i')
 
 stateButton1 = True
 stateButton2 = True
-board.digital[10].write(0)
+
 
 while True:
-    stateButton1 = figmaButton.read()
-    stateButton2 = notionButton.read()
+    Button1 = figmaButton.read()
+    Button2 = notionButton.read()
     
-    if stateButton1 == 1:
+    if Button1 == 0 and stateButton1:
         board.digital[10].write(1)
-        print("button 1 pressed")
+        os.system("start chrome {}".format(figma))
         sleep(1)
         board.digital[10].write(0)
-    elif stateButton2 == 1:
+    if Button2 == 0 and stateButton2:
         board.digital[9].write(1)
-        print("button 2 pressed")
+        os.system("start chrome {}".format(notion))
         sleep(1)
         board.digital[9].write(0)
